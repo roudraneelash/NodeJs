716fe79ff948f2abfd3a5b4bb20f758fc04e7be3
roudranee$ git config --global user.name roudranee <ash.roudraneel@gmail.com>
2023-02-05 20:04:37 +0530

    Beginign nodJs basics

--- MyContactsApp/app.js
+++ MyContactsApp/app.js
@@ -3,16 +3,76 @@ const path = require("path"); //path module to use dynamic url address
 const app = express();
 const port = 3000;
 
+//importing from config
+const db = require("./config/mongoose");
+//importing from model/mongoose schema
+const Contact = require("./models/contact");
+//middleware to read urlencoded data
+
+app.use(express.urlencoded()); //body parser
+app.use(express.static("assets")); //include static files
+
+/*middleware example
+app.use(function (req, res, next) {
+  console.log("middleware called");
+  next();
+  //if  the next is not called ,the control will not be passed to the next route controller
+});
+*/
+
 //set the default view engine to ejs template
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// array of js objects containing the contacts
+
+const arr = [];
+
 //routes
 app.get("/", (req, res) => {
-  res.send("Home page");
+  return res.render("home", {
+    title: "Home",
+    body: "Rendering from EJS Template !!",
+  });
 });
-app.get("/ejs", (req, res) => {
-  res.render("home");
+//displaying contacts
+
+app.get("/contact", (req, res) => {
+  Contact.find({}, (err, contacts) => {
+    if (err) {
+      console.log("Error in db");
+      return;
+    }
+    return res.render("contact", {
+      title: "Contacts Page",
+      arr: contacts,
+    });
+  });
+});
+//displaying input form page
+//managing post route
+app.post("/create-entry", (req, res) => {
+  if (req.body.name != "" && req.body.phone != "") {
+    Contact.create(
+      {
+        name: req.body.name,
+        phone: req.body.phone,
+      },
+      (err, newContact) => {
+        if (err) console.log(err);
+      }
+    );
+  }
+
+  return res.redirect("contact"); //redirecting contact page
+});
+
+app.get("/delete-contact/", (req, res) => {
+  let id = req.query.id;
+  Contact.findByIdAndDelete(id, (err) => {
+    if (err) console.log("found error in db");
+  });
+  res.redirect("back");
 });
 
 app.listen(port, () => {
--- MyContactsApp/readMe.md
+++ MyContactsApp/readMe.md
@@ -2,3 +2,44 @@ Create a contact list app , using express
 
 1.implementing routing
 2.implementing EJS view engine template
+3.implementing get routes , rendering contact lists using ejs temp
+4.implementing input form , reading data , using urlencoded()==> body parser
+5.implementing post route
+6.implementing middleware, the MW function is to go through MW before passing the control to get/post route
+7.implemented Express.js req.params/req.query Property
+
+<!-- .........///////////.................. -->
+
+rendering loops or conditions in ejs template
+<% %>
+<%= %> //for printing any value
+
+<!-- ................Steps......................... -->
+
+1.Create app.js
+2.npm init , to setup up our project
+3.include express , npm i express
+4.start a server (require express, listen on port)
+5.sending data to ejs template(npm i ejs)
+6.use ejs as view engine
+7.path of views
+8.created ejs file
+9.render out page through the route/controller function
+10.include middleware(app.use(express.urlencoded())) where it reads the data and parses it.into keys and value pair
+11.form+post request
+12.handling static files 13. use app.use(express.static('assests'))//paths of the static folders
+14.in ejs file , the path starts from assets , href='/css/contact.css'
+
+\\...........Express req.params/query ............//
+
+params
+<a href="/delete-contact/<%= arr[i].phone%>">
+app.get("/delete-contact/:phone", (req, res) => {
+console.log(req.params);
+});
+
+//query
+<a href="/delete-contact/?phone=<%= arr[i].phone%>">
+app.get("/delete-contact/", (req, res) => {
+console.log(req.query);
+});
